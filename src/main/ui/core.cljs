1d42d30182dff0afde1362216db4ae04cd4195c4
sid597 <siddharthdv77@gmail.com>
2023-11-08 14:12:51 +0530

    fix: minor bug for setup

--- src/main/ui/core.cljs
+++ src/main/ui/core.cljs
@@ -1,7 +1,14 @@
 (ns ui.core
   (:require [reagent.core :as r]
-            [reagent.dom :as rdom]))
+            [reagent.dom :as rd]))
+
+(defn menu []
+  (.addCommand js/document.roamAlphaAPI.ui.blockContextMenu
+    (fn [e]
+      (js/console.log "Hello from roam cljs plugin boilerplate!")
+      (js/console.log e))))
 
 
 (defn init []
-  (js/console.log "Hello from roam cljs plugin boilerplate!"))
+  (js/console.log "Hello from roam cljs plugin boilerplate!")
+  (rd/render [menu] (.getElementById js/document "app")))
