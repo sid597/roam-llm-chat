f19ebe33e1a859c66bdf1510edc257fb8f4f0ac0
sid597 <siddharthdv77@gmail.com>
2023-11-08 16:55:09 +0530

    add command to context menu

--- src/main/ui/core.cljs
+++ src/main/ui/core.cljs
@@ -1,14 +1,25 @@
 (ns ui.core
   (:require [reagent.core :as r]
+            [applied-science.js-interop :as j]
             [reagent.dom :as rd]))
 
-(defn menu []
-  (.addCommand js/document.roamAlphaAPI.ui.blockContextMenu
-    (fn [e]
-      (js/console.log "Hello from roam cljs plugin boilerplate!")
-      (js/console.log e))))
+(defn log
+  [& args]
+  (apply js/console.log args))
+
+
+(defn add-new-option-to-context-menu []
+  (let [block-context-menus (j/get-in js/window [:roamAlphaAPI :ui :blockContextMenu])]
+    (log "block context menu" block-context-menus)
+    (.addCommand block-context-menus
+      (clj->js {:label "Chat LLM: Hello from ClojureScript"
+                :display-conditional (fn [e]
+                                       #_(log "display conditional" e)
+                                       true)
+                :callback (fn [e]
+                            (log "chat llm callback" e))}))))
 
 
 (defn init []
   (js/console.log "Hello from roam cljs plugin boilerplate!")
-  (rd/render [menu] (.getElementById js/document "app")))
+  (add-new-option-to-context-menu))
