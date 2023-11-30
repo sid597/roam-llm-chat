e2581a1fe8280df4c7d2c40148ba237255618f47
sid597 <siddharthdv77@gmail.com>
2023-11-30 22:39:05 +0530

    on context menu when clicked create the message and context block, if already exist render UI

--- src/main/ui/core.cljs
+++ src/main/ui/core.cljs
@@ -3,12 +3,52 @@
             [applied-science.js-interop :as j]
             ["@blueprintjs/core" :as bp :refer [Button InputGroup Card]]
             [ui.render-comp :as rc]
+            [ui.extract-data :refer [q]]
             [reagent.dom :as rd]))
 
 
 
 (defn log
   [& args]  (apply js/console.log args))
+
+(defn create-new-block-with-id [parent-uid block-uid order string]
+  (println "create new block" parent-uid)
+  (j/call-in js/window [:roamAlphaAPI :data :block :create]
+    (clj->js {:location {:parent-uid parent-uid
+                         :order       order}
+              :block    {:uid    block-uid
+                         :string string
+                         :open   true}})))
+
+
+(defn create-blocks [puid cb]
+  (let [m-uid (j/call-in js/window [:roamAlphaAPI :util :generateUID])
+        c-uid (j/call-in js/window [:roamAlphaAPI :util :generateUID])
+        cc-uid (j/call-in js/window [:roamAlphaAPI :util :generateUID])]
+    (-> (create-new-block-with-id puid m-uid 0 "Messages")
+        (.then (fn [] (create-new-block-with-id puid c-uid 1 "Context")))
+        (.then (fn [] (create-new-block-with-id c-uid cc-uid 0 "")))
+        (.then (fn [] (j/call-in js/window [:roamAlphaAPI :data :block :update]
+                                 (clj->js {:block    {:uid    puid
+                                                      :open   false}}))))
+        (.finally (fn [] (js/setTimeout cb 200))))))
+
+
+(defn children-exist? [puid]
+  (let [children (sort-by :order (:children
+                                   (ffirst (q '[:find (pull ?e [:block/string :block/uid :block/order {:block/children ...}])
+                                                :in $ ?uid
+                                                :where
+                                                [?e :block/uid ?uid]]
+
+
+                                             puid))))
+        messages? (= "Messages" (-> children first :string))
+        context?  (= "Context" (-> children second :string))]
+    (cljs.pprint/pprint children)
+    (println "children exist" children messages? context?)
+    (println "msg" (-> children first))
+    (and messages? context?)))
 
 
 (defn add-new-option-to-context-menu []
@@ -26,8 +66,10 @@
                :callback (fn [e]
                            (let [block-uid (j/get e :block-uid)
                                  dom-id (str "block-input-" (j/get e :window-id) "-" block-uid)]
-                             (println "cui" (str "{{roam/render: ((C_s8CL875)) \"C_s8CL875\" " dom-id " " "}}"))
-                             (rc/main {:block-uid block-uid} "filler"  dom-id)
+                              (println "cui" (str "{{roam/render: ((C_s8CL875)) \"C_s8CL875\" " dom-id " " "}}"))
+                              (if (children-exist? block-uid)
+                               (rc/main {:block-uid block-uid} "filler"  dom-id)
+                               (create-blocks block-uid #(rc/main {:block-uid block-uid} "filler"  dom-id)))
                              #_(j/call-in js/window [:roamAlphaAPI :data  :block :update]
                                  (clj->js {:block
                                            {:uid block-uid
