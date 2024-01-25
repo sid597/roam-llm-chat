6921f833639fc978344123b04d7b5e8fad240cae
sid597 <siddharthdv77@gmail.com>
2024-01-25 15:28:10 +0530

    moved to new namespace

--- src/main/ui/components/bottom_bar.cljs
+++ src/main/ui/components/bottom_bar.cljs
@@ -1,43 +1,9 @@
 (ns ui.components.bottom-bar
   (:require [cljs.core.async.interop :as asy :refer [<p!]]
             [cljs.core.async :as async :refer [<! >! go chan put! take! timeout]]
-            [reagent.core :as r]
+            [ui.components.graph-overview-ai :refer [filtered-pages-button]]
             [ui.utils :refer [log get-open-page-uid get-block-parent-with-order get-focused-block create-struct gen-new-uid default-chat-struct get-todays-uid]]
-            [applied-science.js-interop :as j]
             ["@blueprintjs/core" :as bp :refer [ControlGroup Checkbox Tooltip HTMLSelect Button ButtonGroup Card Slider Divider Menu MenuItem Popover MenuDivider]]))
-
-
-(defn filtered-pages-button []
-  (let [disabled? (r/atom true)]
-    (fn []
-      (let [mutation-callback (fn mutation-callback [mutations observer]
-                                (println "mutation callback bottom bar")
-                                (doseq [mutation mutations]
-                                  (when (= (.-type mutation) "childList")
-                                    (doseq [node (array-seq (.-addedNodes mutation))]
-                                      (let [parent-class (j/get node :className)
-                                            child-class (j/get-in node [:firstChild :className])]
-                                        (when (and (instance? js/Element node)
-                                                (or (= "roam-body-main"
-                                                      parent-class)
-                                                  (= "rm-graph-view"
-                                                    parent-class)))
-                                          (if (or (= "rm-graph-view"
-                                                     child-class)
-                                                (= "rm-graph-view"
-                                                  parent-class))
-                                            (reset! disabled? false)
-                                            (reset! disabled? true))))))))
-            star-observing    (let [observer (js/MutationObserver. mutation-callback)]
-                                (.observe observer js/document #js {:childList true
-                                                                    :subtree true}))])
-      (println "--disabled?--" disabled?)
-      [:> Button {:minimal true
-                  :small true
-                  :disabled @disabled?
-                  :on-click #(js/alert "clicked")}
-       "Load filtered pages into chat"])))
-
 
 
 (defn bottom-bar-buttons []
---
+++ src/main/ui/components/graph_overview_ai.cljs
@@ -0,0 +1,42 @@
+(ns ui.components.graph-overview-ai
+  (:require [reagent.core :as r]
+            [applied-science.js-interop :as j]
+            [ui.actions.graph-overview-ai :refer [create-chat-ui-blocks-for-selected-overview]]
+            ["@blueprintjs/core" :as bp :refer [ControlGroup Checkbox Tooltip HTMLSelect Button ButtonGroup Card Slider Divider Menu MenuItem Popover MenuDivider]]))
+
+
+(defn filtered-pages-button []
+  (let [disabled? (r/atom true)]
+    (fn []
+      (let [mutation-callback (fn mutation-callback [mutations observer]
+                                (doseq [mutation mutations]
+                                  (when (= (.-type mutation) "childList")
+                                    (doseq [node (array-seq (.-addedNodes mutation))]
+                                      (let [parent-class (j/get node :className)
+                                            child-class (j/get-in node [:firstChild :className])]
+                                        (when (and (instance? js/Element node)
+                                                (or (= "roam-body-main"
+                                                      parent-class)
+                                                  (= "rm-graph-view"
+                                                    parent-class)))
+                                          (if (or (= "rm-graph-view"
+                                                    child-class)
+                                                (= "rm-graph-view"
+                                                  parent-class))
+                                            (reset! disabled? false)
+                                            (reset! disabled? true))))))))
+            star-observing    (let [observer (js/MutationObserver. mutation-callback)]
+                                (.observe observer js/document #js {:childList true
+                                                                    :subtree true}))])
+      [:> Button {:class-name "chat-with-filtered-pages"
+                  :minimal true
+                  :small true
+                  :disabled @disabled?
+                  :on-click (fn []
+                              (let [chat-block-uid (j/call-in js/window [:roamAlphaAPI :util :generateUID])
+                                    context-block-uid (j/call-in js/window [:roamAlphaAPI :util :generateUID])]
+                                ;(println "chat-block-uid" chat-block-uid context-block-uid)
+                                (create-chat-ui-blocks-for-selected-overview
+                                  chat-block-uid
+                                  context-block-uid)))}
+       "Load filtered pages into chat"])))
