d2c0e513440d18e3d92090a774557ebcd8c4be4d
sid597 <siddharthdv77@gmail.com>
2024-02-06 23:35:47 +0530

    extract block data only when zoomed in on a block, now daily notes chat is put under a AI chats block, utility to get extract data for blocks only, new struct for blocks under AI chats and to provide a default context structure

--- src/main/ui/components/bottom_bar.cljs
+++ src/main/ui/components/bottom_bar.cljs
@@ -2,10 +2,10 @@
   (:require [cljs.core.async.interop :as asy :refer [<p!]]
             [cljs.core.async :as async :refer [<! >! go chan put! take! timeout]]
             [ui.components.quick-buttons :refer [button-with-settings]]
+            [ui.extract-data.chat :refer [data-for-blocks]]
             [ui.components.graph-overview-ai :refer [filtered-pages-button]]
             [ui.utils :refer [ai-block-exists? chat-ui-with-context-struct uid->title log get-child-of-child-with-str-on-page get-open-page-uid get-block-parent-with-order get-focused-block create-struct gen-new-uid default-chat-struct get-todays-uid]]
             ["@blueprintjs/core" :as bp :refer [ControlGroup Checkbox Tooltip HTMLSelect Button ButtonGroup Card Slider Divider Menu MenuItem Popover MenuDivider]]))
-
 
 
 
@@ -22,8 +22,21 @@
                                 (let [chat-block-uid (gen-new-uid)
                                       open-page-uid (<p! (get-open-page-uid))
                                       page-title    (uid->title open-page-uid)
-                                      context-struct [{:s (str "[[" page-title "]]")}]
+                                      block-data    (when (nil? page-title)
+                                                      (str
+                                                        "```"
+                                                        (clojure.string/join "\n -----" (data-for-blocks [open-page-uid]))
+                                                        "```"))
+                                      page-data     (str "[[" page-title "]]")
+                                      context-struct [{:s (if (nil? page-title)
+                                                            block-data
+                                                            page-data)}
+                                                      {:s ""}]
                                       ai-block? (ai-block-exists? open-page-uid)]
+
+                                  (println "open page uid" open-page-uid)
+                                  (println "page title" page-title)
+                                  (println "extract block" block-data)
                                   (if (some? ai-block?)
                                     (create-struct
                                       (default-chat-struct chat-block-uid nil nil context-struct)
@@ -41,23 +54,28 @@
                   :small true
                   :on-click (fn [e]
                               (let [chat-block-uid (gen-new-uid)
-                                    chat-struct (default-chat-struct chat-block-uid)]
-                                (create-struct
-                                  chat-struct
-                                  (get-todays-uid)
-                                  chat-block-uid
-                                  true)))}
+                                    ai-block?      (ai-block-exists? (get-todays-uid))]
+                                (if (some? ai-block?)
+                                  (create-struct
+                                    (default-chat-struct chat-block-uid)
+                                    ai-block?
+                                    chat-block-uid
+                                    true)
+                                  (create-struct
+                                    (chat-ui-with-context-struct chat-block-uid)
+                                    (get-todays-uid)
+                                    chat-block-uid
+                                    true))))}
 
        "Start chat in daily notes, show in sidebar"]
       [:> Divider]
       [:> Button {:minimal true
                   :small true
                   :on-click (fn [e]
-
                               (let [chat-block-uid (gen-new-uid)
                                     [parent-uid
-                                     block-order]  (get-block-parent-with-order (get-focused-block))
-                                    chat-struct (default-chat-struct chat-block-uid nil block-order)]
+                                     block-order] (get-block-parent-with-order (get-focused-block))
+                                    chat-struct   (chat-ui-with-context-struct chat-block-uid nil nil block-order)]
                                 (create-struct
                                   chat-struct
                                   parent-uid
--- src/main/ui/extract_data/chat.cljs
+++ src/main/ui/extract_data/chat.cljs
@@ -1,7 +1,7 @@
 (ns ui.extract-data.chat
   (:require
     [applied-science.js-interop :as j]
-    [ui.utils :as utils :refer [replace-block-uids q uid-to-block get-eid]]
+    [ui.utils :as utils :refer [uid->eid replace-block-uids q uid-to-block get-eid]]
     [clojure.string :as str]))
 
 (def skip-blocks-with-string #{"{{ chat-llm }}" "AI chats"})
@@ -24,17 +24,30 @@
                              (replace-block-uids))])))
 
 
-(defn get-children-for [p]
-  (let [eid               (if (int? p) p (get-eid p))
-        raw-children-data (ffirst (q '[:find (pull ?eid [{:block/children ...} :block/string :block/order])
-                                       :in $ ?eid]
-                                    eid))
-        sorted-by-children (sort-by :order (:children raw-children-data))
-        plain-text         (replace-block-uids (str/join "\n " (extract-strings {:children sorted-by-children})))
-        map-format         (build-map  sorted-by-children)]
 
-    {:plain-text plain-text
-     #_#_:map-format map-format}))
+(defn get-children-for
+  ([p] (get-children-for p false))
+  ([p block?]
+   (let [eid               (cond
+                             (int? p) p
+                             block?   (uid->eid p)
+                             :else    (get-eid p))
+         raw-children-data (ffirst (q '[:find (pull ?eid [{:block/children ...} :block/string :block/order])
+                                        :in $ ?eid]
+                                     eid))
+         sorted-by-children (sort-by :order (:children raw-children-data))
+         block-string        (:string raw-children-data)
+         plain-text         (replace-block-uids (str/join "\n " (concat
+                                                                  (when block-string
+                                                                    [block-string])
+                                                                  (extract-strings {:children sorted-by-children}))))
+         map-format         (build-map  sorted-by-children)]
+     {:plain-text plain-text
+      #_#_:map-format map-format})))
+
+(comment
+ (get-children-for "1owvT89TK" true)
+ (get-children-for "llm chat"))
 
 (defn extract-ref-data-for [title ref-eid ref-data]
   (let [crumbs-set (into #{} (map #(when (not-empty %)
@@ -102,10 +115,26 @@
                              (print "\n"))))))))
     @res))
 
-(data-for-pages [{:text "llm chat"}] (atom false))
+
+
+(defn data-for-blocks [block-uids]
+  (let [res (atom [])]
+    (doall
+      (for [block-uid block-uids]
+        (swap! res (fn [old-res]
+                     (let [block-data (:plain-text (get-children-for block-uid true))]
+                        (conj old-res
+                            (with-out-str
+                              (print "\n")
+                              (print block-data)
+                              (print "\n"))))))))
+    @res))
 
 
 (comment
+  (data-for-blocks ["1owvT89TK"])
+  (data-for-pages [{:text "llm chat"}] (atom true))
+
   (data-for-pages [
                    {:text
                     "[[EVD]] - siRNA silenced IRSp53 significantly reduced internalized 10kDa TMR Dextran, while siRNA silenced Swip1 did not in MDA-MB-231 cells. - [[@moreno-layseca2021cargospecific]]",
