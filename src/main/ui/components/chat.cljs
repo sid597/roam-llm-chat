b9a1f8b0ba1e6c3010cdaa2e48940197f98cc818
sid597 <siddharthdv77@gmail.com>
2024-03-05 13:09:18 +0530

    remove scroll maybe causing issues in rendering

--- src/main/ui/components/chat.cljs
+++ src/main/ui/components/chat.cljs
@@ -27,10 +27,7 @@
                                #_(println "5. chat context block rendered successfully")))
                            (.catch (fn [e]
                                      (log "Error in chat context block" (:uid @context) e @context))))
-                         (p "Scroll chat to bottom")
-                         (.scrollTo context-el 0 (.-scrollHeight context-el))
-                         (.scrollTo context-el 0 (.-scrollHeight context-el))
-                         (.scrollTo context-el 0 (.-scrollHeight context-el))))]
+                         (p "Scroll chat to bottom")))]
      (r/create-class
        {:component-did-mount  update-fn
         :component-did-update update-fn
